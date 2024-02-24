53ea454780e4f09e36f4dba4008a5f56157be436
khcoder88 <kevinhang88880@gmail.com>
2024-02-24 10:46:04 -0500

    added tables and code to allow for adding and viewing posts and comments

--- back/dummyPostData.sql
+++ back/dummyPostData.sql
@@ -9,3 +9,5 @@ INSERT INTO comments(post_id, user_id, parent_comment_id, content, time)
 VALUES (1, 3, NULL, 'This is the first comment on the first post.', DATETIME('now')),
        (2, 1, NULL, 'Commenting on the second post.', DATETIME('now')),
        (1, 2, 1, 'Reply to the first comment.', DATETIME('now'));
+
+

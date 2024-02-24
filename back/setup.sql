9d22bfb9b261a1d93ea354240fd1a09f1682f889
sndadda <s.dadda@yahoo.com>
2024-02-24 00:37:23 -0500

    added database schema

--- back/setup.sql
+++ back/setup.sql
@@ -8,3 +8,34 @@ CREATE TABLE tokens (
     token TEXT,
     username TEXT
 );
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    marker_id VARCHAR(255),
+    subject VARCHAR(255),
+    content TEXT,
+    timestamp TIMESTAMP,
+    likes INT DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY,
+    post_id INTEGER,
+    user_id INTEGER,
+    content TEXT,
+    timestamp TIMESTAMP,
+    likes INT DEFAULT 0,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE markers (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    name VARCHAR(255),
+    latitude FLOAT,
+    longitude FLOAT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
